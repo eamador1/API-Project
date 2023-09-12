e1b4cffe663413fb5cc23eb7721696b2207fc39c
erwin amador <eamador0305@gmail.com>
2023-09-12 14:31:37 -0600

    fix linter errors, except 2

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import populateSeries from './modules/home.js';
 
 const axios = require('axios');
 
-axios.get(`https://api.tvmaze.com/shows`)
-  .then(response => {
+axios.get('https://api.tvmaze.com/shows')
+  .then((response) => {
     const series = response.data;
+
     populateSeries(series);
   })
   .catch(error => {
     console.error(error);
   });
-
