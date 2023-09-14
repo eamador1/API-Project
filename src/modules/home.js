d3d4c3e5212966dd440f09b7c48ff69d17730a3c
erwin amador <eamador0305@gmail.com>
2023-09-14 14:12:19 -0600

    post likes in involvement API

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,7 +1,11 @@
+import postLikes from './addLikes.js';
 import { updateLikes } from './fetchLikes.js';
 
+
+const seriesContainer = document.querySelector('.series-container');
+
 const populateSeries = (series) => {
-  const seriesContainer = document.querySelector('.series-container');
+  
   seriesContainer.innerHTML = '';
 
   series.forEach((seriesData) => {
@@ -12,31 +16,58 @@ const populateSeries = (series) => {
     const contButtons = document.createElement('div');
     const comments = document.createElement('button');
     const reservations = document.createElement('button');
+    const likeButton = document.createElement('button');
     const displayLikes = document.createElement('span');
 
     image.classList.add('series-image');
     contTitle.classList.add('contTitle');
     contButtons.classList.add('contButtons');
     comments.classList.add('comments');
+    likeButton.classList.add('likeButton');
     displayLikes.classList.add('displayLikes');
     comments.textContent = 'Comments';
     reservations.classList.add('reservations');
     reservations.textContent = 'Reservations';
     image.src = seriesData.image.original;
     title.textContent = seriesData.name;
+    likeButton.innerHTML='&#9825;';
+    likeButton.setAttribute('data-series-data', JSON.stringify(seriesData));
+
 
     displayLikes.id = `likes-${seriesData.id}`;
     updateLikes(seriesData.id, displayLikes);
+    
 
     contTitle.appendChild(title);
     contButtons.appendChild(comments);
     contButtons.appendChild(reservations);
     contGeneral.appendChild(image);
     contGeneral.appendChild(contTitle);
+    contGeneral.appendChild(likeButton);
     contGeneral.appendChild(displayLikes);
     contGeneral.appendChild(contButtons);
     seriesContainer.appendChild(contGeneral);
+
+    
+
+
   });
+
+  
+
+  
 };
 
 export default populateSeries;
+
+
+
+seriesContainer.addEventListener('click', async (event) => {
+    if (event.target.classList.contains('likeButton')) {
+        const seriesData = event.target.dataset.seriesData;
+        await postLikes(seriesData.id);
+        const showLikes = seriesContainer.querySelector('.displayLikes');
+      await updateLikes(seriesData.id, showLikes);
+      console.log(updateLikes);
+    }
+  })
