225ae070afd2ce8ff5bd9d39d3875a28e7f37adf
erwin amador <eamador0305@gmail.com>
2023-09-15 09:06:32 -0600

    Fix linter errors

--- src/modules/seriesCounter.js
+++ src/modules/seriesCounter.js
@@ -1,6 +1,6 @@
 const itemsCounter = (items) => {
-    const shows = document.querySelector('.menu-series');
-    shows.innerHTML = `Series <span>(${items})</span>`;
-  };
-  
-  export default itemsCounter;
+  const shows = document.querySelector('.menu-series');
+  shows.innerHTML = `Series <span>(${items})</span>`;
+};
+
+export default itemsCounter;
